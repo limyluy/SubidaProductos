83e5138e1a118e74a1a03fc5302b12935fcbc9b6
limyluy001 <juantry001@gmail.com>
2019-03-15 22:47:25 -0500

    haceido pruebas para llenar el rcv segun la necesesidad

---
+++ app/src/main/java/com/example/subidaproductos/Adaptadores/ClienteAdaptador.java
@@ -0,0 +1,51 @@
+package com.example.subidaproductos.Adaptadores;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.subidaproductos.Entidades.Cliente;
+import com.example.subidaproductos.R;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+public class ClienteAdaptador extends FirestoreRecyclerAdapter<Cliente, ClienteAdaptador.ClienteViewHolder> {
+    Context  context;
+
+    public ClienteAdaptador(@NonNull FirestoreRecyclerOptions<Cliente> options, Context context) {
+        super(options);
+        this.context = context;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull ClienteViewHolder holder, int position, @NonNull Cliente model) {
+        holder.nombre.setText(model.getNombre());
+        holder.descripcion.setText("numero de locales: ");
+        holder.numero.setText(String.valueOf(model.getLocales().size()));
+    }
+
+    @NonNull
+    @Override
+    public ClienteViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(context).inflate(R.layout.item_main,viewGroup,false);
+        return new ClienteViewHolder(v);
+    }
+
+    class ClienteViewHolder extends RecyclerView.ViewHolder{
+        TextView nombre;
+        TextView descripcion;
+        TextView numero;
+
+        public ClienteViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nombre = itemView.findViewById(R.id.txt_nombre_item);
+            descripcion = itemView.findViewById(R.id.txt_detalle_item);
+            numero = itemView.findViewById(R.id.txt_numero_item);
+        }
+    }
+}
--- app/src/main/java/com/example/subidaproductos/MainActivity.java
+++ app/src/main/java/com/example/subidaproductos/MainActivity.java
@@ -1,14 +1,76 @@
 package com.example.subidaproductos;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.subidaproductos.Adaptadores.ClienteAdaptador;
+import com.example.subidaproductos.Entidades.Cliente;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnNuevoCliente;
+    Context  context;
+    FirebaseFirestore db;
 
+
+    ClienteAdaptador adaptadorCliente;
+    RecyclerView rcvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       db = FirebaseFirestore.getInstance();
+
+        btnNuevoCliente = findViewById(R.id.btn_crear_cliente);
+        context = getApplicationContext();
+
+        rcvMain = findViewById(R.id.rcv_main);
+        rcvMain.setHasFixedSize(true);
+        rcvMain.setLayoutManager(new LinearLayoutManager(this));
+
+        CollectionReference reference = db.collection("clientes");
+
+        Query query = reference.orderBy("nombre", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<Cliente> options = new FirestoreRecyclerOptions.Builder<Cliente>()
+                .setQuery(query,Cliente.class)
+                .build();
+
+        adaptadorCliente = new ClienteAdaptador(options,context);
+
+        btnNuevoCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verClientesBaseDatos();
+            }
+        });
+
+    }
+
+    private void verClientesBaseDatos() {
+        rcvMain.setAdapter(adaptadorCliente);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adaptadorCliente.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adaptadorCliente.stopListening();
     }
 }
